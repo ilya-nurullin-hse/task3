c052883dec9a095a97277e5f1dfc40c76401fc41
Ilya Nurullin <nurullinin@mail.ru>
2017-07-21 21:05:04 +0500

    The first version of solution.

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task3
 {
@@ -10,6 +6,54 @@ namespace Task3
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                var defaultConsoleColor = Console.ForegroundColor;
+
+                Console.WriteLine("Введите координаты точки в формате float через пробел или q для выхода");
+                var input = Console.ReadLine();
+
+                if (input == "")
+                    continue;
+
+                if (input == "q")
+                    break;
+
+                try
+                {
+                    var coords = input.Split();
+                    var x = float.Parse(coords[0]);
+                    var y = float.Parse(coords[1]);
+
+                    var isXMoreThan0 = x >= 0;
+                    var isPointInCircle = x * x + y * y <= 1;
+                    var isInArea1 = isXMoreThan0 && isPointInCircle;
+
+                    var upperLine = x >= -2 && x <= 0 && 2 * y <= (x + 2);
+                    var lowerLine = x >= -2 && x <= 0 && 2 * y >= -(x + 2);
+                    var isInArea2 = upperLine && lowerLine;
+
+                    if (isInArea1 || isInArea2)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Точка принадлежит закрашенной области");
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("Точка не принадлежит закрашеннгой области");
+                    }
+                    Console.ForegroundColor = defaultConsoleColor;
+                }
+                catch (FormatException)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Ожидались числа в формате float или q");
+                    Console.ForegroundColor = defaultConsoleColor;
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
